e528cb44ea57d22d86b488908f38ee016144d3f0
PresnoArmweak <preston.armstrong1234@gmail.com>
2026-03-04 10:13:22 -0600

    orderSum text

--- app/src/main/java/com/example/campuscoffeeorder/OrderSum.kt
+++ app/src/main/java/com/example/campuscoffeeorder/OrderSum.kt
@@ -1,12 +1,15 @@
 package com.example.campuscoffeeorder
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class OrderSum : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,12 @@ class OrderSum : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val coffeeType = findViewById<TextView>(R.id.coffeeType)
+
+
+        val typeText = intent.getStringExtra("coffeeType")
+
+        coffeeType.text = " $typeText"
     }
 }
