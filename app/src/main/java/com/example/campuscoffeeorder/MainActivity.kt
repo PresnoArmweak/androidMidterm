33811c009c7a8a66c51487acb69cad89820ab69a
PresnoArmweak <Preston.armstrong1234@gmail.com>
2026-03-08 21:33:02 -0500

    loging

--- app/src/main/java/com/example/campuscoffeeorder/MainActivity.kt
+++ app/src/main/java/com/example/campuscoffeeorder/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.campuscoffeeorder
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -13,9 +14,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity" // filter in logcat to not see extra tag:MainActivity onCreate | onStart | onResume | onPause | onStop | onDestroy
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d(TAG, "onCreate: Activity is being created")
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -49,5 +52,29 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: Activity is becoming visible")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: Activity is in the foreground and interactive")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: Activity is partially obscured or losing focus")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: Activity is no longer visible")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: Activity is being destroyed")
     }
 }
